61348a13b5e1c825f0ea95d57101e1256c8e1f58
ht0201 <43738511+ht0201@users.noreply.github.com>
2022-07-29 11:36:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = () => {
+    fetch("https://swapi.py4e.com/api/films/")
+      .then((res) => res.json())
+      .then((data) => {
+        const tranformedMovies = data.results.map((movieData) => ({
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        }));
+        setMovies(tranformedMovies);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
